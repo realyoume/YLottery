c2582dd7a2f668f641672e455ed3c11aecbb14dc
realyoume <realyoume@163.com>
2023-08-06 20:40:05 +0800

    feat: 15-gateway

---
+++ ylottery-gateway/src/main/java/com/yumi/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.yumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @version 1.0
+ * @author: xk
+ * @description TODO
+ * @date: 2023/8/6 19:42
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
+
+
+    
